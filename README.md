19150dc42ac861ecf063dfa82af3e89ef0897a35
Tomaz Podgorsek <tomaz.podgorsek@gmail.com>
2022-01-11 11:53:09 +0100

    First post (Meet Quarkus)

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# articles
-Source code from articles on tomscode.com
+## Posts
+This repository contains source code related to blog posts published on tomscode.com
---
+++ meet-quarkus/build.gradle.kts
@@ -0,0 +1,35 @@
+plugins {
+    java
+    id("io.quarkus")
+}
+
+repositories {
+    mavenCentral()
+    mavenLocal()
+}
+
+val quarkusPlatformGroupId: String by project
+val quarkusPlatformArtifactId: String by project
+val quarkusPlatformVersion: String by project
+
+dependencies {
+    implementation(enforcedPlatform("${quarkusPlatformGroupId}:${quarkusPlatformArtifactId}:${quarkusPlatformVersion}"))
+    implementation("io.quarkus:quarkus-resteasy")
+    implementation("io.quarkus:quarkus-resteasy-qute")
+    implementation("io.quarkus:quarkus-arc")
+    testImplementation("io.quarkus:quarkus-junit5")
+    testImplementation("io.rest-assured:rest-assured")
+}
+
+group = "com.tomscode"
+version = "master-SNAPSHOT"
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_11
+    targetCompatibility = JavaVersion.VERSION_11
+}
+
+tasks.withType<JavaCompile> {
+    options.encoding = "UTF-8"
+    options.compilerArgs.add("-parameters")
+}
